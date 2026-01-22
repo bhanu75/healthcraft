e4227e116758673753456c724ead147836b37c9a
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2026-01-22 09:33:28 +0530

    Update WeightGraph.jsx

--- src/modules/WeightTracker/WeightGraph.jsx
+++ src/modules/WeightTracker/WeightGraph.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { 
-  LineChart, 
-  Line, 
   XAxis, 
   YAxis, 
   CartesianGrid, 
   Tooltip, 
   ResponsiveContainer,
   Area,
-  AreaChart
+  AreaChart,
+  Line
 } from 'recharts';
 import { formatShortDate } from '../../utils/helpers';
 
